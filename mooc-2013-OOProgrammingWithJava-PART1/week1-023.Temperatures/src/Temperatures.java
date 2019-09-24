b607779b31b5952d9bb58ee805e3e1b1d36c307a
dustincheung <dustincnj@gmail.com>
2019-09-23 21:07:49 -0400

    completed week1

--- mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
@@ -7,13 +7,15 @@ public class Temperatures {
 
         Scanner reader = new Scanner(System.in);
         // Write your code here. 
+        while(true){
+            System.out.print("Input temperature: ");
+            double temp = Double.parseDouble(reader.nextLine());
 
-        // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
-        // Remove or comment out these lines above before trying to run the tests.
+            if(temp < -30 || temp >40){
+                continue;
+            }else {
+                Graph.addNumber(temp);
+            }
+        }
     }
 }
